3a30aeb34f5e814bc6aa5093c6accc7561f10cf5
Cristian Soria <crissoria07@gmail.com>
2024-10-21 15:00:15 -0300

    BASE - routing y paginas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { ButtonsComponent } from './pages/buttons/buttons.component';
+import { CardsComponent } from './pages/cards/cards.component';
+import { FormsComponent } from './pages/forms/forms.component';
+import { ImagesComponent } from './pages/images/images.component';
+import { TextsComponent } from './pages/texts/texts.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'buttons', component: ButtonsComponent },
+  { path: 'cards', component: CardsComponent },
+  { path: 'forms', component: FormsComponent },
+  { path: 'images', component: ImagesComponent },
+  { path: 'texts', component: TextsComponent },
+];
